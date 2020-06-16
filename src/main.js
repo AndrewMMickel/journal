89f56a58573326d7a845b1cc1650c47930df21d4
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-06-16 10:45:50 -0700

    made 2nd js and readmemd

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import { Entry } from './scripts.js';
+
+$(document).ready(function () {
+  $("#journalEntry").submit(function (event) {
+    event.preventDefault();
+
+    var title = $("#titleInput").val();
+    var text = $("#journalTextInput").val();
+    var newEntry = new Entry(title, text);
+    var wordCount = newEntry.numWords(text);
+    var vowelCount = newEntry.numVowels();
+    var consonantCount = newEntry.numConsonants();
+    var teaser = newEntry.getTeaser();
+    $(".ttlWords").text(wordCount);
+    $(".ttlVowels").text(vowelCount);
+    $(".ttlConsonants").text(consonantCount);
+    $(".teaser").text(teaser);
+    $(".output").show();
+  });
+});
