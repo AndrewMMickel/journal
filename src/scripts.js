d7b95ddfff3cc96d71b3c0fd899959f3bb4c0e65
Andrew Mickel <andrew.m.mickel@gmail.com>
2020-06-17 09:34:51 -0700

    created prototype for getTeaser

--- src/scripts.js
+++ src/scripts.js
@@ -16,3 +16,14 @@ Entry.prototype.numConsonants = function () {
   var consonants = this.text.match(/[bcdfghjklmnpqrstvxzwy]/gi);
   return consonants === null ? 0 : consonants.length;
 }
+
+Entry.prototype.getTeaser = function () {
+  var sentenceSplit = this.text.replace(/([.?!])\s*(?=[A-Z])/g, "$1|").split("|")[0];
+  var words = this.numWords(sentenceSplit);
+  var firstEightWords = sentenceSplit.split(' ');
+  if (words > 8) {
+    return firstEightWords.slice(0, 8).join(" ");
+  } else {
+    return sentenceSplit;
+  }
+}
